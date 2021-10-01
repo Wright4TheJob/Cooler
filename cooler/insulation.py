f515dff2c07bb56c442efae2bce67040eb191686
David Wright <djwright3.14@gmail.com>
2021-10-01 10:56:55 -0400

    Refactored with property dectorators and added smaller classes.

--- cooler/insulation.py
+++ cooler/insulation.py
@@ -1,59 +1,59 @@
-class Box:
-    def __init__(self):
-        self.length = 1
-        self.width = 1
-        self.height = 1
-        self.side = self.length*self.height
-        self.front = self.width*self.height
-        self.top = self.length*self.width
-        self.surface_area = 2*(self.side + self.front+self.top)
+class Wall:
+    def __init__(self, thickness, k, length=None, width=None,
+                surface_area = None):
+        self.t = thickness
+        self.k = k
+        self.length = length
+        self.width = width
+        self.__surface_area = surface_area
 
-        self.misc_inflow = 0 # W/K
+    @property
+    def surface_area(self):
+        if self.length and self.width:
+            return self.length * self.width
 
-        self.k = 1 # W/m*K
-        self.ins_thickness = 1 # m
-        self.conductance = 1 #W/C
-        self.calculate_conductance()
+    @property
+    def conductance(self):
+        return self.k*self.surface_area/self.t
 
-    def set_insulation_k(self,new_k):
-        self.k = new_k
-        self.calculate_conductance()
+class Box_From_Walls:
+    def __init__(self, walls, misc_inflow = 0):
+        self.walls = walls
+        self.misc_inflow = misc_inflow
 
-    def calculate_conductance(self):
-        insulation_conductance = self.k*self.surface_area/self.ins_thickness # W/K
-        self.conductance = insulation_conductance + self.misc_inflow
+    @property
+    def surface_area(self):
+        return sum([w.surface_area for w in self.walls])
 
-    def set_insulation_thick(self, new_t):
-        self.ins_thickness = new_t
-        self.calculate_conductance()
+    @property
+    def conductance(self):
+        return sum([w.conductance for w in self.walls]) + self.misc_inflow
 
-    def set_length(self, l):
-        self.length = l
-        self.calculate_surface_area()
-        self.calculate_conductance()
+class Box_Uniform_Insulation:
+    def __init__(self, length, width, height, thickness, k, misc_inflow = 0):
+        self.length = length
+        self.width = width
+        self.height = height
+        self.thickness = thickness # m
+        self.k = k # W/m*K
+        self.side = Wall(self.thickness, self.k, length = self.length, width = self.height)
+        self.front = Wall(self.thickness, self.k, length = self.width, width = self.height)
+        self.top = Wall(self.thickness, self.k, length = self.length, width = self.width)
+        self.walls = [self.side, self.side, self.front, self.front, self.top, self.top]
+        self.misc_inflow = misc_inflow # W/K
+        self.box = Box_From_Walls(self.walls, self.misc_inflow)
 
-    def set_width(self, w):
-        self.width = w
-        self.calculate_surface_area()
-        self.calculate_conductance()
+    @property
+    def conductance(self):
+        return self.box.conductance
+    @property
+    def surface_area(self):
+        return self.box.surface_area
 
-    def set_height(self, h):
-        self.height = h
-        self.calculate_surface_area()
-        self.calculate_conductance()
-
-    def calculate_surface_area(self):
-        self.side = self.length*self.height
-        self.front = self.width*self.height
-        self.top = self.length*self.width
-        self.surface_area = 2*(self.side + self.front+self.top)
-
-    def set_misc_conductance(self, c):
-        self.misc_inflow = c
-        self.calculate_conductance()
-
-    def get_conductance_kW_per_C(self):
+    @property
+    def conductance_kW_per_C(self):
         return self.conductance/1000
 
-    def get_conductance_W_per_C(self):
+    @property
+    def conductance_W_per_C(self):
         return self.conductance
--- test/test_insulation_model.py
+++ test/test_insulation_model.py
@@ -9,44 +9,52 @@ import pytest
 
 def test_basic_box_surface_area():
     expected =  6
-    assert ins.Box().surface_area == expected
+    assert ins.Box_Uniform_Insulation(1, 1, 1, 1, 1).surface_area == expected
 
 def test_basic_box_insulation():
     expected =  6
-    assert ins.Box().conductance == expected
+    assert ins.Box_Uniform_Insulation(1, 1, 1, 1, 1).conductance == expected
 
 def test_update_box_k_val():
     expected =  3
-    box = ins.Box()
-    box.set_insulation_k(0.5)
+    box = ins.Box_Uniform_Insulation(1, 1, 1, 1, 0.5)
     assert box.conductance == expected
 
 def test_update_box_insulation_thickness():
     expected = 12
-    box = ins.Box()
-    box.set_insulation_thick(0.5)
+    box = ins.Box_Uniform_Insulation(1, 1, 1, 0.5, 1)
     assert box.conductance == expected
 
 def test_update_length():
     expected = 10
-    box = ins.Box()
-    box.set_length(2)
+    box = ins.Box_Uniform_Insulation(2, 1, 1, 1, 1)
     assert box.conductance == expected
 
 def test_update_width():
     expected = 10
-    box = ins.Box()
-    box.set_width(2)
+    box = ins.Box_Uniform_Insulation(1, 2, 1, 1, 1)
     assert box.conductance == expected
 
 def test_update_height():
     expected = 10
-    box = ins.Box()
-    box.set_height(2)
+    box = ins.Box_Uniform_Insulation(1, 1, 2, 1, 1)
     assert box.conductance == expected
 
 def test_updated_misc_heat_flow():
     expected = 8
-    box = ins.Box()
-    box.set_misc_conductance(2)
+    box = ins.Box_Uniform_Insulation(1, 1, 1, 1, 1, misc_inflow = 2)
+    assert box.conductance == expected
+
+def test_wall_conductance():
+    expected = 0.5
+    wall = ins.Wall(2, 1, length=1, width=1)
+    assert wall.surface_area == 1 # m^2
+    assert wall.conductance == expected
+
+def test_insulator_from_walls():
+    expected = 3
+    wall = ins.Wall(2, 1, length=1, width=1)
+    walls = [wall]*6
+    box = ins.Box_From_Walls(walls)
+    assert box.surface_area == 6 # m^2
     assert box.conductance == expected
